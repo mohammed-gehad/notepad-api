7caa3d50d211b93428e39ecdd45f8f4c6c0ccd3d
mohammedgehad <pu2k@hotmail.com>
2020-04-27 04:39:22 +0530

    delete note

--- Router/NoteRoute.js
+++ Router/NoteRoute.js
@@ -6,33 +6,36 @@ const NoteModel = mongoose.model("Note");
 const UserModel = mongoose.model("User");
 //auth is a middleware for verifying the token
 const auth = require("../middleware/auth");
+const _ = require("lodash");
 
 // getting note by id
 route.get("/:id", auth, async (req, res) => {
-    let notes
+  let notes;
   try {
-    if(_id) notes = await NoteModel.find({_id:req.params.id}).populate('author',['username' , 'email']);
-    else notes = await NoteModel.find().populate('author',['username' , 'email']);
+    if (_id)
+      notes = await NoteModel.find({ _id: req.params.id }).populate("author", [
+        "username",
+        "email",
+      ]);
+    else
+      notes = await NoteModel.find().populate("author", ["username", "email"]);
     res.send(notes);
   } catch (e) {
     res.send(e);
   }
 });
 
-
-
 //adding a new note
-route.post("/",auth, async (req, res) => {
+route.post("/", auth, async (req, res) => {
   try {
-      //title and content of the new note
+    //title and content of the new note
     const { title, content } = req.body;
-    const note = new NoteModel({ title, content, author:req.user._id })
-    const user =await UserModel.findOne({_id:req.user._id})
-    user.note=[...user.note,note._id]
-    await note.save()
-    await user.save()
-    res.send(note)
-
+    const note = new NoteModel({ title, content, author: req.user._id });
+    const user = await UserModel.findOne({ _id: req.user._id });
+    user.note = [...user.note, note._id];
+    await note.save();
+    await user.save();
+    res.send(note);
   } catch (e) {
     console.log(e);
     res.send(e);
@@ -41,32 +44,39 @@ route.post("/",auth, async (req, res) => {
 //
 
 //delete a note
-route.delete("/",auth, async (req, res) => {
+route.delete("/:id", auth, async (req, res) => {
   try {
-      //note id to be deleted
-      const {_id}=req.body
-      const note =await NoteModel.findOneAndDelete({_id});
-      const user = await UserModel.findById({_id:req.user._id})
-      user.note.pull(_id)
-      user.save()
-      note.save()
-      res.send(user.note)
-    
+    //note id to be deleted
+    const _id = req.params.id;
+
+    // await NoteModel.find();
+    await NoteModel.findByIdAndDelete({
+      _id: mongoose.Types.ObjectId(_id),
+    });
+    await UserModel.updateOne(
+      { _id: mongoose.Types.ObjectId(req.user._id) },
+      { $pull: { note: mongoose.Types.ObjectId(_id) } }
+    );
+    res.end();
   } catch (e) {
     res.send(e);
   }
 });
 
 //update a note
-route.put("/",auth, async (req, res) => {
-    try {
-        //note id to be updated
-        const {_id,title,content}=req.body
-        const note =await NoteModel.findOneAndUpdate({_id},{title,content},{new:true});
-        res.send(note)
-    } catch (e) {
-      res.send(e);
-    }
-  });
+route.put("/", auth, async (req, res) => {
+  try {
+    //note id to be updated
+    const { _id, title, content } = req.body;
+    const note = await NoteModel.findOneAndUpdate(
+      { _id },
+      { title, content },
+      { new: true }
+    );
+    res.send(note);
+  } catch (e) {
+    res.send(e);
+  }
+});
 
 module.exports = route;

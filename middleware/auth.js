43c10399554dc016a0a3e9b871327543a12ea26d
mohammedgehad <pu2k@hotmail.com>
2020-04-27 05:49:30 +0530

    env var

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('config')
 
  module.exports = function(req,res,next){
     const token = req.header('token')
-    const decoded = jwt.decode(token,config.get('jwtSecret'))
+    const decoded = jwt.decode(token,process.env.jwtSecret)
     if(!decoded)res.send('invalid token')
     else{
         req.user = decoded
